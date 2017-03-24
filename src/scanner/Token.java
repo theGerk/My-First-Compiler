1925635066a1a3c968561e27b4b52c1d2ae4e937
Benji Altman <benji@altmansoftwaredesign.com>
2017-03-23 19:10:13 -0500

    added javadoc on Token

--- src/scanner/Token.java
+++ src/scanner/Token.java
@@ -6,14 +6,26 @@
 package scanner;
 
 /**
+ * Represents a Token read by the scanner.
  *
  * @author Benji
  */
 public class Token {
 
+	/**
+	 * Type of the Token
+	 */
 	private TokenType type;
-	private String input;
+	/**
+	 * string given to create the token
+	 */
+	private final String input;
 
+	/**
+	 * Determines token's type based off it's string
+	 *
+	 * @param key String of the token
+	 */
 	public Token(String key) {
 		input = key;
 		type = LookUp.lookUp.get(key);
@@ -22,24 +34,52 @@ public class Token {
 		}
 	}
 
+	/**
+	 * Sets a number token, by passing in a number and the string version that
+	 * was read.
+	 *
+	 * @param key Could be any number, supposed to be the number that was read
+	 * @param stringForm String version of the number
+	 */
 	public Token(int key, String stringForm) {
 		type = TokenType.NUM;
 		input = stringForm;
 	}
 
+	/**
+	 * Gets the string that created the Token
+	 *
+	 * @return string form of token
+	 */
 	public String getLexeme() {
 		return input;
 	}
 
+	/**
+	 * Gets a formated version of the token
+	 *
+	 * @return human readable string representing a token
+	 */
 	@Override
 	public String toString() {
 		return "Token: " + this.input;
 	}
 
+	/**
+	 * Gets the type of token that it is
+	 *
+	 * @return it's TokenType
+	 */
 	public TokenType getType() {
 		return type;
 	}
 
+	/**
+	 * overload of equals for comparing another token type
+	 *
+	 * @param compareAgainst TokenType to compare against
+	 * @return if they have the same token type
+	 */
 	public boolean equals(TokenType compareAgainst) {
 		return compareAgainst == getType();
 	}
