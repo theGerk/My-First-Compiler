556f6125145950d053e2bad9da5ba26f7e1aeed7
Benji Altman <benji@altmansoftwaredesign.com>
2017-03-23 19:31:01 -0500

    added javadoc on LookUp

--- src/scanner/LookUp.java
+++ src/scanner/LookUp.java
@@ -8,16 +8,24 @@ package scanner;
 import java.util.HashMap;
 
 /**
+ * maps from a string representation of token to TokenType To use simply use the
+ * public lookup inside of it.
  *
  * @author Benji
  */
 public class LookUp extends HashMap<String, TokenType> {
 
+	/**
+	 * An array of strings corresponding to the TokenType enumerable.
+	 */
 	private static String[] keywordArray = new String[]{
 		"and", "array", "begin", "div", "do", "else", "end", "function", "if", "integer", "mod", "not", "of", "or", "procedure", "program", "real", "then", "var", "while",
 		";", ",", ".", ":", "[", "]", "(", ")", "+", "-", "=", "<>", "<", ">", "<=", ">=", "*", "/", ":="
 	};
 
+	/**
+	 * Private default constructor, set's up the map.
+	 */
 	private LookUp() {
 
 		//init the lookup
@@ -26,5 +34,8 @@ public class LookUp extends HashMap<String, TokenType> {
 		}
 	}
 
-	public static LookUp lookUp = new LookUp();
+	/**
+	 * Global lookup to be used in all cases
+	 */
+	public final static LookUp lookUp = new LookUp();
 }
