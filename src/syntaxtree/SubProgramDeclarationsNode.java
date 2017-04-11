855ceaf60737a39127ceef741bc26507924d1375
Benji Altman <benji@altmansoftwaredesign.com>
2017-04-10 22:20:20 -0500

    ?

--- src/syntaxtree/SubProgramDeclarationsNode.java
+++ src/syntaxtree/SubProgramDeclarationsNode.java
@@ -1,27 +1,28 @@
-
 package syntaxtree;
 
 import java.util.ArrayList;
 
 /**
  * Represents a collection of subprogram declarations
+ *
  * @author Erik Steinmetz
  */
 public class SubProgramDeclarationsNode extends SyntaxTreeNode {
-    
-    private ArrayList<SubProgramNode> procs = new ArrayList<SubProgramNode>();
-    
-    public void addSubProgramDeclaration( SubProgramNode aSubProgram) {
-        procs.add( aSubProgram);
-    }
-    
-    public String indentedToString( int level) {
-        String answer = this.indentation( level);
-        answer += "SubProgramDeclarations\n";
-        for( SubProgramNode subProg : procs) {
-            answer += subProg.indentedToString( level + 1);
-        }
-        return answer;
-    }
-    
+
+	private ArrayList<SubProgramNode> procs = new ArrayList<SubProgramNode>();
+
+	public void addSubProgramDeclaration(SubProgramNode aSubProgram) {
+		procs.add(aSubProgram);
+	}
+
+	@Override
+	public String indentedToString(int level) {
+		String answer = this.indentation(level);
+		answer += "SubProgramDeclarations\n";
+		for (SubProgramNode subProg : procs) {
+			answer += subProg.indentedToString(level + 1);
+		}
+		return answer;
+	}
+
 }
