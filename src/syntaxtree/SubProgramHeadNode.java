942e5cb7722ea6bfc4e7f46bf23b3ce261a279e1
Benji Altman <benji@altmansoftwaredesign.com>
2017-04-17 03:06:07 -0500

    only if and while left

--- src/syntaxtree/SubProgramHeadNode.java
+++ src/syntaxtree/SubProgramHeadNode.java
@@ -1,21 +1,21 @@
 package syntaxtree;
 
 import java.util.ArrayList;
+import symboltable.Scope;
 
 /**
  * Created by Benji on 4/14/2017.
  */
 public class SubProgramHeadNode extends SyntaxTreeBase implements IPublicName {
-	
-	private final String   name;
+
+	private final String name;
 	private final String[] arguments;
-	
+
 	public SubProgramHeadNode(String name, ArrayList<String> arguments) {
 		this.name = name;
 		this.arguments = (String[]) arguments.toArray();
 	}
-	
-	
+
 	/**
 	 * Creates a String representation of this node and its children.
 	 *
@@ -31,14 +31,30 @@ public class SubProgramHeadNode extends SyntaxTreeBase implements IPublicName {
 		}
 		return build.toString();
 	}
-	
+
 	public final String[] getArguments() {
 		return arguments;
 	}
-	
+
 	@Override
-	public String getName(){
+	public String getName() {
 		return name;
 	}
-}
 
+	@Override
+	protected String toMips(Scope symbolTable, String indent) {
+		StringBuilder build = new StringBuilder(indent).append("#SubProgramHeadNode\n");
+
+		//load ptr to previous stack's head to pick up values
+		build.append(indent).append("lw $t0, 4($sp)\n");
+		build.append(indent).append("lw $t0, ($t0)\n");
+
+		//iterate across arguments
+		for (int i = 0; i < arguments.length; i++) {
+			build.append(indent).append("lw $t1, ").append((i + 1) * 4).append("($t0)\t#get loaded value from stack\n");
+			build.append(indent).append("sw $t1, ").append(symbolTable.getMemoryOffset(arguments[i])).append("($sp)\t#save word in correct spot (hopefully?)\n");	//TODO confirm where arguments are added to declared vars
+		}
+
+		return build.toString();
+	}
+}
